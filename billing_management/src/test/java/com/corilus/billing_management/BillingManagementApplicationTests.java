0e0eff8d02082f495ad51eeee2b4e5e88763e889
hani0216 <hanimedyouni12@gmail.com>
2025-04-17 14:25:29 +0100

    basic files

---
+++ billing_management/src/test/java/com/corilus/billing_management/BillingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corilus.billing_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
